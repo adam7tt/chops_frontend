77e4aa54e191099ab7786972df366835bd9b49cc
Natan Vargas <33994997+nvarg@users.noreply.github.com>
2019-12-07 17:52:21 -0800

    Merged Visualizations Removed Timeline

--- src/views/Professor-profile/Visualizations/Visualization.vue
+++ src/views/Professor-profile/Visualizations/Visualization.vue
@@ -1,46 +1,26 @@
 <template>
-<<<<<<< HEAD
     <div class="visualization">
             <stat-gram
                 :size='500'
                 :stats='[1,1,1,1,1,1]'
                 :max = '[5,5,5,5,5,5]'
                 :num='5'/>
-            <word-cloud style='flex-shrink: 1;'/>
+            <word-cloud />
            <!-- <timeline/> -->
     </div>
-=======
-    <div id="visualization">
-        <div>
-            <word-cloud/>
-        </div>
-        <div>
-            <!-- <timeline/> -->
-        </div>
-    </div>   
->>>>>>> 4b0ebeaa4747c3cf451b4d9902f0d693ef5bd1cf
 </template>
 
 <script>
     import WordCloud from '@/views/Professor-profile/Visualizations/Word-cloud.vue';
-<<<<<<< HEAD
-    import Timeline from '@/views/Professor-profile/Visualizations/Timeline.vue';
+//    import Timeline from '@/views/Professor-profile/Visualizations/Timeline.vue';
     import StatGram from '@/views/Professor-profile/Visualizations/StatGram.vue';
-=======
-    //import Timeline from '@/views/Professor-profile/Visualizations/Timeline.vue';
->>>>>>> 4b0ebeaa4747c3cf451b4d9902f0d693ef5bd1cf
 
     export default {
         name: 'visualization',
         components:{
-<<<<<<< HEAD
             WordCloud,
-            Timeline,
+             // Timeline,
             StatGram
-=======
-            WordCloud
-           // Timeline
->>>>>>> 4b0ebeaa4747c3cf451b4d9902f0d693ef5bd1cf
         }
     }
 </script>
