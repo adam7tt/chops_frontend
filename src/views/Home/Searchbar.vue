c092d08d65f086989d980425159c75271bdb6b67
nvarg <natvargas97@gmail.com>
2020-04-22 12:13:50 -0700

    searchbar adv search impl

--- src/views/Home/Searchbar.vue
+++ src/views/Home/Searchbar.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="searchbar-p">
       <div v-if="!viewAdv" class="searchbar">
-        <input @keydown.enter="getResults" type="text" v-model="input"/>
+        <input @keydown.enter="getResults" type="text" v-model="dict.search"/>
         <button @click="getResults">Search</button>
         <button @click.prevent="viewAdvanced">Advanced Search</button>
       </div>
@@ -14,21 +14,21 @@
           </div>
           <div class="row">
               <div class="col s12">
-                <input disabled value="To be added." v-model="keywords" type="text"/>
+                <input v-model="keywords" type="text"/>
                 <label for="keywords">Keywords (For multiple terms the form is ("Fruit Loops" "Health")</label>
               </div>
           </div>
           <div class="row">
               <div class="col s6">
-                  <input v-model="dict.university_name" type="text"/>
+                  <input v-model="dict.university" type="text"/>
                   <label for="Univeristy">University</label>
               </div>
               <div class="col s6">
-                    <input v-model="dict.department_name" type="text"/>
+                    <input v-model="dict.department" type="text"/>
                   <label for="department">Department</label>
               </div>
           </div>
-        <button @click="getAdvResults">Search</button>
+        <button @click="getResults">Search</button>
         <button @click="viewAdvanced">Simple</button>
       </div>
   </div>
@@ -40,15 +40,11 @@ import axios from 'axios'
 export default {
     data() {
         return {
-            input: null,
-            name: null,
-            keywords: [],
-            university: null,
-            department: null,
-            data : [],
+            keywords: "",
             viewAdv: false,
             con_string: 'http://127.0.0.1:5000/academics/',
             dict : {
+                search: null,
                 name: null,
                 keywords: [],
                 university_name: null,
@@ -58,28 +54,20 @@ export default {
     },
     methods: {
         getResults() {
-            axios.get("http://127.0.0.1:5000/academics/?search=" + this.input)
-            .then(res => {
-                const data = res.data.results
-                this.$router.push({ name: 'Results', params: {data: data} })
-            })
-        },
-        getAdvResults(){
-            for (const property in this.dict) {
-                if(this.dict[property] != null && this.con_string.slice(-1) != '/'){
-                    this.con_string += '&?' + property + '=' + this.dict[property];
-                }else if(this.dict[property] != null){
-                    this.con_string += '?' + property + '=' + this.dict[property];
-                }
-            }
-            axios.get(this.con_string)
-            .then(res => {
+            axios.get("http://127.0.0.1:5000/academics/", {
+                params: this.dict,
+            }).then(res => {
                 const data = res.data.results
                 this.$router.push({ name: 'Results', params: {data: data} })
             })
         },
         viewAdvanced(){
             this.viewAdv = !this.viewAdv
+        }
+    },
+    watch: {
+        keywords: function() {
+            this.dict["keywords"] = this.keywords.match(/\w+|"[^"]+"/g).join(",").split('"').join("")
         }
     }
 };
